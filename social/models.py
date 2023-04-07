e41c6b1503a9b8efafc09551188e256f5f075dde
Lautaro Jara <jara.lautaro@gmail.com>
2023-04-07 17:10:00 -0300

    Tercera pre-entrega

--- social/models.py
+++ social/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='Foto_perfil_German.jpeg')
+	image = models.ImageField(default='Foto_perfil_Generica.png')
 
 	def __str__(self):
 		return f'Perfil de {self.user.username}'
